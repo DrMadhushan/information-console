bcf4a2f8002b909bcdf21b7f39d379cd3ea2158b
DrMadhushan <dev.drmad@gmail.com>
2022-02-18 23:52:42 +0530

    include customized virtual keyboard

--- server.js
+++ server.js
@@ -13,13 +13,28 @@ server.use(bodyParser.json()); // parse form data client
 server.use(express.static(path.join(__dirname, "/src/public"))); // configure express to use public folder handle css + js files
 
 server.get("/", async function (req, res) {
-  // res.json({ hello: "world" });
   let images = ["1.jpg", "2.jpg", "3.jpg"];
-  res.render("home", {images: images});
+  res.render("home", { images: images });
 });
-
 server.get("/about", async function (req, res) {
   res.render("about");
+});
+server.get("/userLogin", async function (req, res) {
+  await new Promise((resolve) => setTimeout(resolve, 1000));
+  res.render("package/login");
+});
+server.get("/info", async function (req, res) {
+  res.render("info/help");
+});
+server.get("/adminLogin", async function (req, res) {
+  res.render("settings/login");
+});
+server.get("/explore", async function (req, res) {
+  // res.render("explore/explore");
+  res.render("explore/searchResults");
+});
+server.get("*", async function (req, res) {
+  res.status(404).render("404");
 });
 
 module.exports = server;
