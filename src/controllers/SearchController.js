16653891d17fabcdd0ab66241b1841a9c2e0b9c6
DrMadhushan <dev.drmad@gmail.com>
2022-05-18 06:29:31 +0530

    add node fetch

--- src/controllers/SearchController.js
+++ src/controllers/SearchController.js
@@ -1,9 +1,21 @@
+const fetch = require('node-fetch');
+const apiRoot = "https://127.0.0.1:8000/api";
+
+fetch('https://example.com')
+  .then(response => response.json())
+  .then(data => {
+    console.log(data)
+  })
+
 const home = async (req, res) => {
   res.render("explore/explore");
 };
 
 const searchItem = async (req, res) => {
   await new Promise((resolve) => setTimeout(resolve, 1000));
+
+  const apiUrl = apiRoot + "/"
+
   console.log(req.body);
   let products = ["prod1"];
   res.render("explore/searchResults", { products: products });
