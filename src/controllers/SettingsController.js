f8b98b633feee0b46d1a2209f6a20eaad86e4bc8
DrMadhushan <dev.drmad@gmail.com>
2022-11-20 23:35:27 +0530

    add scaled locker functionoality

--- src/controllers/SettingsController.js
+++ src/controllers/SettingsController.js
@@ -1,8 +1,10 @@
 const formidable = require("formidable");
 const auth = require("../authService");
+const axios = require("axios");
 const path = require("path");
 const config = require("../../config.json");
 const uploadFolder = path.join(__dirname, "../public/uploads");
+const apiRoot = config.api.address;
 
 async function login(req, res) {
   res.render("settings/login");
@@ -11,8 +13,31 @@ async function signin(req, res) {
   console.log(req.body.uname);
   console.log(req.body.pwd);
   console.log("logged? " + auth.adminLogin(req.body.uname, req.body.pwd));
+  //
   if (auth.adminLogin(req.body.uname, req.body.pwd)) {
-    res.render("settings/settings");
+    query = apiRoot + "/locker";
+    lockers = [];
+    const response = await axios.get(query).catch(function (error) {
+      console.log("Error when getting locker details");
+      console.log(error);
+    });
+    if (response.status == 200) {
+      response.data.forEach((locker_elem) => {
+        if (locker_elem.orders != null) {
+          lockers.push({
+            locker_no: locker_elem.id,
+            package_no: locker_elem.orders.id,
+          });
+        } else {
+          lockers.push({
+            locker_no: locker_elem.id,
+            package_no: "Empty",
+          });
+        }
+      });
+      console.log("order response = ", response);
+      res.render("settings/settings", { lockers: lockers });
+    }
   } else {
     res.render("settings/login");
   }
